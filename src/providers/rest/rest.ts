42042ff99e6e7d06477ed705e80bf76b09917583
guialvesmartins <guilhermealmartins@gmail.com>
2018-12-11 11:22:12 -0200

    Add

---
+++ src/providers/rest/rest.ts
@@ -0,0 +1,43 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class RestProvider {
+
+  apiUrl = 'https://jsonplaceholder.typicode.com';
+
+  constructor(public http: HttpClient) {
+    console.log('Hello RestProvider Provider');
+  }
+
+  getUsers() {
+    return new Promise(resolve => {
+      this.http.get(this.apiUrl+'/users').subscribe(data => {
+        resolve(data);
+      }, err => {
+        console.log(err);
+      });
+    });
+  }
+
+  getTodos() {
+    return new Promise(resolve => {
+      this.http.get(this.apiUrl+'/todos').subscribe(data => {
+        resolve(data);
+      }, err => {
+        console.log(err);
+      });
+    });
+  }
+
+  getPhotos() {
+    return new Promise(resolve => {
+      this.http.get(this.apiUrl+'/photos').subscribe(data => {
+        resolve(data);
+      }, err => {
+        console.log(err);
+      });
+    });
+  }
+  
+}
